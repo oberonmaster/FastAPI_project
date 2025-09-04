5d2dda6cf77983e2c247e3c3119907b0085373f0
dmitriy <oberon@mail.ru>
2025-09-04 23:18:14 +0300

    update gitignore update main.py - start app and uvicorn

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+"""
+Основной скрипт создания и запуска приложения
+"""
+
+import uvicorn
+
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+def main():
+    """Запуск сервера"""
+    uvicorn.run(app)
 
 
 if __name__ == '__main__':
-    pass
+    main()
