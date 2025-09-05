fddad600406d6157950d3ba4bdb35787c4e3c86b
dmitriy <oberon@mail.ru>
2025-09-05 21:32:33 +0300

    add database module

---
+++ app/database.py
@@ -0,0 +1,18 @@
+""" Интерфейс настройки базы данных и подключения """
+
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+
+load_dotenv()
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
